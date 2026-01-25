c95926747d29c8fbe4640803546c321803073c48
Prasad-Bhoite19 <prasadsb2002@gmail.com>
2026-01-25 14:08:00 +0530

    Save local changes before pulling

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.devops_ec2.public_ip
-}
-
-output "ssh_command" {
-  description = "Command to SSH into EC2 instance"
-  value       = "ssh -i <your-private-key.pem> ubuntu@${aws_instance.devops_ec2.public_ip}"
-}
